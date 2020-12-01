37055831a3a50728fb2c7562b142d0de636b070d
Gaëlle AUGE <gaelle.auge95@gmail.com>
2020-12-01 17:22:01 +0100

    dev inscription + connexion + roles utilisateurs + début backoffice

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,23 +4,82 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\AuthenticatedVoter;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Event\AuthenticationEvent;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/inscription", name="security_registration")
      */
-    public function registration(): Response
+    public function registration(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder, AuthorizationCheckerInterface $authChecker): Response
     {
+        // Si l'internaute est connecté, il n'a rien à faire sur la route '/connexion', on le redirige vers la route '/blog'
+        if ($authChecker->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute('blog');
+        }
         $user = new User;
 
         $formRegistration = $this->createForm(RegistrationType::class, $user);
 
+        $formRegistration->handleRequest($request);
+
+        if ($formRegistration->isSubmitted() && $formRegistration->isValid()) {
+            // encodePassword() est une méthode issue de l'interface UserPasswordEncoderInterface permettant d'encoder le mot de passe
+            // Pour encoder le mot de passe, il faut que notre User implémente l'interface UserInterface et nous devons déclarer obligatoirement 5 méthodes dans l'entité: getPassword(), getUsername(), getSalt(), getRoles() et eraseCredentials()
+            $hash = $encoder->encodePassword($user, $user->getPassword());
+
+            $user->setPassword($hash); // on envoie le mot de passe haché dans l'entité $user
+
+            $manager->persist($user);
+            $manager->flush();
+
+            return $this->redirectToRoute('security_login');
+        }
+
+
         return $this->render('security/registration.html.twig', [
             'formRegistration' => $formRegistration->createView()
         ]);
+    }
+
+    /**
+     * @Route("/connexion", name="security_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils, AuthorizationCheckerInterface $authChecker): Response
+    {
+        // Si l'internaute est connecté, il n'a rien à faire sur la route '/connexion', on le redirige vers la route '/blog'
+        if ($authChecker->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute('blog');
+        }
+
+        //Récupération du message d'erreur en cas de mauvaise connexion
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Récupération du dernier username (email) saisi par l'internaute en cas d'erreur
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUsername
+        ]);
+    }
+
+    /**
+     * @Route("/deconnexion", name="security_logout")
+     */
+    public function logout()
+    {
+        // cette méthode ne retourne rien, il nous suffit d'avoir une route pour se déconnecter
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,13 +2,24 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UserRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(
+ *      fields = {"email"},
+ *      message="Un compte est déjà existant à cette adresse email !"
+ * )
+ * @UniqueEntity(
+ *      fields = {"username"},
+ *      message="Ce nom d'utilisateur est déjà existant, veuillez en saisir un nouveau !"
+ * )
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id
@@ -19,20 +30,46 @@ class User
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Veuillez renseigner un Email")
+     * @Assert\Email(message="Veuillez saisir un Email valide")
      */
     private $email;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Veuillez saisir un nom d'utilisateur")
+     * @Assert\Length(
+     *      min="2",
+     *      minMessage="Votre nom d'utilisateur doit contenir minimum 2 caractères"
+     * )
      */
     private $username;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(
+     *      min="8",
+     *      minMessage="Votre mot de passe doit faire minimum 8 caractères"
+     * )
+     * @Assert\EqualTo(
+     *      propertyPath="confirm_password",
+     *      message="Les mots de passe ne correspondent pas"
+     * )
      */
     private $password;
 
+    /**
+     * @Assert\EqualTo(
+     *      propertyPath="password",
+     *      message="Les mots de passe ne correspondent pas"
+     * )
+     */
     public $confirm_password;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
 
     public function getId(): ?int
     {
@@ -71,6 +108,30 @@ class User
     public function setPassword(string $password): self
     {
         $this->password = $password;
+
+        return $this;
+    }
+
+    // cette méthode est uniquement destinée à nettoyer les mots de passe en texte brut éventuellement stockés
+    public function eraseCredentials()
+    {
+    }
+
+    // Renvoit la chaîne de caractères non encodée que l'utilisateur a saisie, qui est utilisée à l'origine pour encoder le mot de passe
+    public function getSalt()
+    {
+    }
+
+    // Cette fonction renvoit un tableau de chaîne de caractères.
+    // Renvoit les rôles accordés à l'utilisateur
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
 
         return $this;
     }
